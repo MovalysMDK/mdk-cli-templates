2da02bea9c058a6acc267e09e6f2d76a2abb3a9b
Quentin LAGARDE <quentin.lagarde@sopra.com>
2015-12-23 17:57:08 +0100

    FIX : Controls

--- ios/MFTemplate/src/controls/MDKControls.h
+++ ios/MFTemplate/src/controls/MDKControls.h
@@ -32,5 +32,6 @@ IB_DESIGNABLE @interface MDKPickerList : MDKUIExternalPickerList @end
 IB_DESIGNABLE @interface MDKSwitch : MDKUIExternalSwitch @end
 IB_DESIGNABLE @interface MDKNumberPicker : MDKUIExternalNumberPicker @end
 IB_DESIGNABLE @interface MDKScanner : MDKUIExternalScanner @end
+IB_DESIGNABLE @interface MDKSignature : MDKUIExternalSignature @end
 
 
