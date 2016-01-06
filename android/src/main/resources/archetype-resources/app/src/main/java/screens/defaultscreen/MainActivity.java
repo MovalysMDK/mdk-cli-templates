287104c4fce48fb003fc7599d2a7f57a77bbbd44
Remy Sanfeliu <remy.sanfeliu@soprasteria.com>
2016-01-06 17:40:32 +0100

    [Android] Refactor : Template now inherits from mdk-android-basic-template

--- android/src/main/resources/archetype-resources/app/src/main/java/screens/defaultscreen/MainActivity.java
+++
@@ -1,8 +0,0 @@
-package ${package}.screens.defaultscreen;
-
-import com.adeuza.movalysfwk.mobile.mf4android.activity.AbstractMMActivity;
-import com.adeuza.movalysfwk.mobile.mf4android.activity.business.displaymain.MFRootActivity;
-
-public class MainActivity extends AbstractMMActivity implements MFRootActivity {
-
-}
